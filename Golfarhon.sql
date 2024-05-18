29b6bd27c553de1db185ffe29ced05e3786611fe
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-18 11:13:37 -0700

    Golfarhon.sql

--- Golfarhon.sql
+++ Golfarhon.sql
@@ -15,54 +15,54 @@ SET NOCOUNT ON;		-- Report only errors
 -- Drop Tables
 -- --------------------------------------------------------------------------------
 IF OBJECT_ID( 'TEventCorporateSponsorshipTypeCorporateSponsors' )	IS NOT NULL DROP TABLE TEventCorporateSponsorshipTypeCorporateSponsors
-IF OBJECT_ID( 'TEventCorporateSponsorshipTypeBenefits' )			IS NOT NULL DROP TABLE TEventCorporateSponsorshipTypeBenefits
-IF OBJECT_ID( 'TEventCorporateSponsorshipTypes' )					IS NOT NULL DROP TABLE TEventCorporateSponsorshipTypes
-IF OBJECT_ID( 'TCorporateSponsors' )								IS NOT NULL DROP TABLE TCorporateSponsors
-IF OBJECT_ID( 'TCorporateSponsorshipTypes' )						IS NOT NULL DROP TABLE TCorporateSponsorshipTypes
+IF OBJECT_ID( 'TEventCorporateSponsorshipTypeBenefits' )		IS NOT NULL DROP TABLE TEventCorporateSponsorshipTypeBenefits
+IF OBJECT_ID( 'TEventCorporateSponsorshipTypes' )			IS NOT NULL DROP TABLE TEventCorporateSponsorshipTypes
+IF OBJECT_ID( 'TCorporateSponsors' )					IS NOT NULL DROP TABLE TCorporateSponsors
+IF OBJECT_ID( 'TCorporateSponsorshipTypes' )				IS NOT NULL DROP TABLE TCorporateSponsorshipTypes
 
-IF OBJECT_ID( 'TBenefits' )											IS NOT NULL DROP TABLE TBenefits
-IF OBJECT_ID( 'TEventGolferTeamandClubs')							IS NOT NULL DROP TABLE TEventGolferTeamandClubs
-IF OBJECT_ID( 'TEventGolferTeams' )									IS NOT NULL DROP TABLE TEventGolferTeams
-IF OBJECT_ID( 'TEventGolferSponsors' )								IS NOT NULL DROP TABLE TEventGolferSponsors
-IF OBJECT_ID( 'TEventGolfers' )										IS NOT NULL DROP TABLE TEventGolfers
+IF OBJECT_ID( 'TBenefits' )						IS NOT NULL DROP TABLE TBenefits
+IF OBJECT_ID( 'TEventGolferTeamandClubs')				IS NOT NULL DROP TABLE TEventGolferTeamandClubs
+IF OBJECT_ID( 'TEventGolferTeams' )					IS NOT NULL DROP TABLE TEventGolferTeams
+IF OBJECT_ID( 'TEventGolferSponsors' )					IS NOT NULL DROP TABLE TEventGolferSponsors
+IF OBJECT_ID( 'TEventGolfers' )						IS NOT NULL DROP TABLE TEventGolfers
 
-IF OBJECT_ID( 'TSponsors' )											IS NOT NULL DROP TABLE TSponsors
-IF OBJECT_ID( 'TEvents' )											IS NOT NULL DROP TABLE TEvents
-IF OBJECT_ID( 'TGolfers' )											IS NOT NULL DROP TABLE TGolfers		
-IF OBJECT_ID( 'TTeamandClubs' )										IS NOT NULL DROP TABLE TTeamandClubs
-IF OBJECT_ID( 'TLevelofTeams' )										IS NOT NULL DROP TABLE TLevelofTeams
+IF OBJECT_ID( 'TSponsors' )						IS NOT NULL DROP TABLE TSponsors
+IF OBJECT_ID( 'TEvents' )						IS NOT NULL DROP TABLE TEvents
+IF OBJECT_ID( 'TGolfers' )						IS NOT NULL DROP TABLE TGolfers		
+IF OBJECT_ID( 'TTeamandClubs' )						IS NOT NULL DROP TABLE TTeamandClubs
+IF OBJECT_ID( 'TLevelofTeams' )						IS NOT NULL DROP TABLE TLevelofTeams
 
-IF OBJECT_ID( 'TGenders' )											IS NOT NULL DROP TABLE TGenders
-IF OBJECT_ID( 'TTypeofTeams' )										IS NOT NULL DROP TABLE TTypeofTeams
-IF OBJECT_ID( 'TShirtSizes' )										IS NOT NULL DROP TABLE TShirtSizes
-IF OBJECT_ID( 'TStates' )											IS NOT NULL DROP TABLE TStates
-IF OBJECT_ID( 'TPaymentStatuses' )									IS NOT NULL DROP TABLE TPaymentStatuses
+IF OBJECT_ID( 'TGenders' )						IS NOT NULL DROP TABLE TGenders
+IF OBJECT_ID( 'TTypeofTeams' )						IS NOT NULL DROP TABLE TTypeofTeams
+IF OBJECT_ID( 'TShirtSizes' )						IS NOT NULL DROP TABLE TShirtSizes
+IF OBJECT_ID( 'TStates' )						IS NOT NULL DROP TABLE TStates
+IF OBJECT_ID( 'TPaymentStatuses' )					IS NOT NULL DROP TABLE TPaymentStatuses
 
-IF OBJECT_ID( 'TPaymentTypes' )										IS NOT NULL DROP TABLE TPaymentTypes 
+IF OBJECT_ID( 'TPaymentTypes' )						IS NOT NULL DROP TABLE TPaymentTypes 
 
 -- --------------------------------------------------------------------------------
 -- Step #1.1: Create Tables
 -- --------------------------------------------------------------------------------
 CREATE TABLE TEvents
 (
-	 intEventID							INTEGER			NOT NULL
-	,dtmEventDate						Date			NOT NULL
+	 intEventID			INTEGER		NOT NULL
+	,dtmEventDate			Date		NOT NULL
 	,CONSTRAINT TEvents_PK PRIMARY KEY ( intEventID )
 )
 
 CREATE TABLE TGolfers
 (
-	 intGolferID						INTEGER			NOT NULL
-	,strFirstName						VARCHAR(255)	NOT NULL
-	,strLastName						VARCHAR(255)	NOT NULL
-	,strAddress							VARCHAR(255)	NOT NULL
-	,strCity							VARCHAR(255)	NOT NULL
-	,intStateID							INTEGER			NOT NULL
-	,strZip								VARCHAR(255)	NOT NULL
-	,strPhoneNumber						VARCHAR(255)	NOT NULL
-	,strEmail							VARCHAR(255)	NOT NULL
-	,intShirtSizeID						Integer			NOT NULL
-	,intGenderID						Integer			NOT NULL
+	 intGolferID			INTEGER		NOT NULL
+	,strFirstName			VARCHAR(255)	NOT NULL
+	,strLastName			VARCHAR(255)	NOT NULL
+	,strAddress			VARCHAR(255)	NOT NULL
+	,strCity			VARCHAR(255)	NOT NULL
+	,intStateID			INTEGER		NOT NULL
+	,strZip				ARCHAR(255)	NOT NULL
+	,strPhoneNumber			VARCHAR(255)	NOT NULL
+	,strEmail			VARCHAR(255)	NOT NULL
+	,intShirtSizeID			Integer		NOT NULL
+	,intGenderID			Integer		NOT NULL
 	,CONSTRAINT TGolfers_PK PRIMARY KEY ( intGolferID )
 )
 
